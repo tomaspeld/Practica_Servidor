3857a1431312ebd61557e991cb4b09b82604b5c5
tomaspeld <tomaspeld@gmail.com>
2019-11-12 21:07:00 -0300

    final practica

--- app.js
+++ app.js
@@ -2,4 +2,22 @@ let http = require("http");
 
 http.createServer(function(req, res) {
 
+res.writeHead(200, {"Content-Type": "text/plain"});
+
+switch(req.url) {
+    case "/":
+        res.end("Bienvenido a mi homepage");
+
+    case "/productos":
+        res.end("Estos son nuestro productos");
+        break;
+
+    default:
+        res.end("Error");
+        break;
+    
+}
+
+res.end("Terminado");
+
 }) .listen(2020, "localhost");
